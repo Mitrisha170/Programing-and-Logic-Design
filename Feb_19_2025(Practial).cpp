1cfb36a3a915d76e796e6ea901711c42ae8497e1
Mitrisha170 <mitrishadas170@gmail.com>
2025-02-27 19:35:36 +0530

    My Work

---
+++ Feb_19_2025(Practial).cpp
@@ -0,0 +1,55 @@
+/*#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int h,k,cal,r;
+	cout << "Enter the first point: ";
+	cin >> h;
+	cout << "Enter the second point: ";
+	cin >> k;
+	cal=pow(h-3,2)+pow(k-2,2);
+	r=5*5;
+	if(cal<r)
+	{
+		cout << "Inside the circle";
+	}
+	else if(cal>r)
+	{
+		cout << "Outside the circle";
+	}
+	else
+	{
+		cout << "On the circle";
+	}
+	return 0;
+}*/
+
+
+/*#include<iostream>
+using namespace std;
+int main()
+{
+	int num[9]={3,5,7,9,5,11,7,13,11};
+	int i,n,k=0;
+	cout << "Enter the number: ";
+	cin >> n;
+	for(i=0;i<9;i++)
+	{
+		if(n==num[i])
+		{
+			k++;
+		}
+	}
+	if (k!=0)
+	{
+		cout << k;
+	}
+	else
+	{
+		cout << "No number";
+	}
+	return 0;
+}*/
+
+
