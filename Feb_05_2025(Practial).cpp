9c00e7af70e3840fe50af97e19fa948a352ce1ed
Mitrisha170 <mitrishadas170@gmail.com>
2025-02-27 19:24:57 +0530

    My Work

---
+++ Feb_05_2025(Practial).cpp
@@ -0,0 +1,60 @@
+/*#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+	char str[20];
+	int i,j,k=0;
+	printf("Enter the string value: ");
+	scanf("%s",&str);
+	j=strlen(str);
+	for(i=0;i<j;i++)
+	{
+		if(isdigit(str[i]))
+		{
+			printf("%c\n",str[i]);
+			k++;
+		}
+    }
+	if(k==0)
+	{
+	printf("No integer.");	
+	}
+	return 0;
+}*/
+
+
+//#include<stdio.h>
+//#include<string.h>
+//#include<ctype.h>
+//int main()
+//{
+//	char str[20];
+//	printf("Enter the string value: ");
+//	scanf("%s",&str);
+//	printf("%s\n",strlwr(str));
+//	printf("%s\n",strupr(str));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<string.h>
+//#include<ctype.h>
+//int main()
+//{
+//	char a[20]="Mitrisha Das",b[20],c[10]="arma1234",d[10];
+//	printf("Enter your name: ");
+//	gets(b);
+//	printf("Enter your password: ");
+//	gets(d);
+//	if(strcmp(a,b)==0 && strcmp(c,d)==0)
+//	{
+//		printf("Login successful");
+//	}
+//	else
+//	{
+//		printf("Invalid login");
+//	}
+//	return 0;
+//}
